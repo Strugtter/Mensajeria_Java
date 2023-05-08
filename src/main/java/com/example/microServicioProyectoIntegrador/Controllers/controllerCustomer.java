717234be4c506a818d7ff04d1593eea369862101
Camilo Botina Trujillo <camilobotinatrujillo@gmail.com>
2023-05-08 21:11:34 +1000

    add permisos de solictudes

--- src/main/java/com/example/microServicioProyectoIntegrador/Controllers/controllerCustomer.java
+++ src/main/java/com/example/microServicioProyectoIntegrador/Controllers/controllerCustomer.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class controllerCustomer {
 
 
